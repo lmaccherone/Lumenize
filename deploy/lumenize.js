d3f77572747e3796c54ef9bc99cd67afd2a4685b
Larry Maccherone <Larry@Maccherone.com>
2012-03-10 13:37:03 -0500

    update docs

--- deploy/lumenize.js
+++ deploy/lumenize.js
@@ -4261,6 +4261,18 @@ require.define("/lumenize.coffee", function (require, module, exports, __dirname
   
   The package is fairly large ~252KB but most of that is the embedded timezone files which compress really well. The Github pages server will gzip 
   the package so it's only ~59KB over the wire.
+  
+  Then at the top of the javascript where you want to call it, put the following:
+  
+  `var lumenize = require('./lumenize');`
+  
+  Then to use it, you can either create local aliases like:
+  
+  `var ChartTime = lumenize.ChartTime;`
+  
+  or you can just use the lumenize namespace:
+  
+  `var stdDev = lumenize.functions.$standardDeviation([20, 30, 50]);
       
   ## Installation for node.js usage ##
   
